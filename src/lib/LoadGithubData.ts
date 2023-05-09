014abaa69c0a3348f702beea3c4a0efa82eb154d
Jeremy Ondrey <jeremy.ondrey@threedy.io>
2023-05-09 17:37:11 +0200

    Change to use types, remove unnecessary functions

--- src/lib/LoadGithubData.ts
+++ src/lib/LoadGithubData.ts
@@ -1,37 +1,20 @@
+import type { Repository, LanguageData } from './types';
+
 const BASEURL = import.meta.env.VITE_BASEURL;
 const USERNAME = import.meta.env.VITE_USERNAME
 const BRANCH_NAME = import.meta.env.VITE_BRANCHNAME
 
-const headers = {
-  Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
-};
-
-export async function loadData(topic: string, amount: number) {
-  const response = await fetch(
-    `${BASEURL}/${USERNAME}repos?topics=${topic}&per_page=${amount}`,
-    { headers }
-  );
-  if (!response.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  const data = await response.json();
-  return data;
-}
-
 export async function loadAllRepos() {
-  // const response = await fetch(`${BASEURL}/${BASEURL}/repos`, {
-  //   headers,
-  // });
   const response = await fetch(`${BASEURL}/users/${USERNAME}/repos`);
   if (!response.ok) {
     throw new Error('Failed to fetch data');
   }
 
-  const data = await response.json();
+  const data:Repository[] = await response.json();
   return data;
 }
 
+//currently unused, but might be useful in the future
 export async function loadRepoLanguage(repo: string) {
   const response = await fetch(
     `${BASEURL}/repos/${USERNAME}/${repo}/languages`
@@ -40,46 +23,23 @@ export async function loadRepoLanguage(repo: string) {
     throw new Error('Failed to fetch data');
   }
 
-  const data = await response.json();
+  const data:LanguageData = await response.json();
   return data;
-}
-
-export async function loadRepoImage(repo: string) {
-  const response = await fetch(
-    `${BASEURL}/repos/${USERNAME}/${repo}/contents/thumb.jpg`,
-    { headers }
-  );
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  const data = await response.json();
-  return data.content;
 }
 
 export async function loadImageWithoutToken(repo: string): Promise<string> {
   const imgUrl = `https://raw.githubusercontent.com/${USERNAME}/${repo}/${BRANCH_NAME}/thumb.jpg`;
-  return fetch(imgUrl)
-    .then((response) => {
-      if (response.ok) {
-        return response.blob();
-      }
-      throw new Error('Network response was not ok.');
-    })
-    .then((blob) => {
-      //convert to base64 so the response can be used the same way as with the loadRepoImage function
-      const reader = new FileReader();
-      reader.readAsDataURL(blob);
-      return new Promise<string>((resolve) => {
-        reader.onloadend = () => {
-          const base64data = reader.result as string;
-          resolve(base64data);
-        };
-      });
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      return "";
-    });
+
+  try {
+    const response = await fetch(imgUrl);
+    if (response.ok) {
+      return imgUrl;
+    } else {
+      throw new Error('Network response was not ok. The repository might not contain a thumb.jpg');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return "error";
+  }
 }
+
