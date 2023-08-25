b2abc46e4c4f78d2b25bf57193941650adb1f647
jondreyThreedy <jeremy.ondrey@threedy.io>
2023-08-25 14:08:18 +0200

    Add daisyUI with custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    daisyui: {
+      themes: [
+        {
+          threedyDraft: {
+            primary: '#641ae6',
+            secondary: '#d926a9',
+            accent: '#00ffac',
+            neutral: '#2a323c',
+            'base-100': '#091227',
+            info: '#3abff8',
+            success: '#36d399',
+            warning: '#fbbd23',
+            error: '#f87272',
+          },
+        },
+      ],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
